6fa92241c6a72956d6bcfcaa3cd84bb1847d9cb3
Alex Peters <alexpeters@Alexs-MacBook-Pro.local>
2021-04-09 11:32:02 -0500

    latest commit

--- analysis.R
+++ analysis.R
@@ -2,24 +2,16 @@
 
 #loading in libraries
 library(ggplot2)
-<<<<<<< HEAD
 library(MASS)
 library(corrplot)
 library(arm)
 library(xgboost)
-=======
-library(corrplot)
-library(car)
-library(MASS)
-library(arm)
->>>>>>> d3115a2761dba4762ef958e54798a7a85d45c7ad
 
 #loading in data
 data = read.csv("football.csv")
 data$Conference = as.factor(data$Conference)
 data$Year = as.factor(data$Year)
 
-<<<<<<< HEAD
 # ################### Explorartory Histograms ##########################
 # ggplot(data, aes(x = Games)) + geom_histogram(binwidth = 1, fill = 4) + 
 #   ggtitle("Histogram of Games Played") + facet_grid(Year ~ .)
@@ -67,144 +59,6 @@ label <- data$Selected #response variable
 removed = c("Team", "Year", "Games", "Selected")
 newdata = data[,! names(data) %in% removed]
 
-=======
-# More histograms
-# Explorartory Histograms
-ggplot(data, aes(x = Games, fill = Year)) + geom_histogram(binwidth = 1) + 
-  ggtitle("Histogram of Games Played")
-ggplot(data, aes(x = X3rdDownConvPct, fill = Conference)) + geom_histogram(binwidth = .02) +
-  ggtitle("Histogram of 3rd Down Conversion %") 
-ggplot(data, aes(x = X3rdDownConvPctDef, fill = Conference)) + geom_histogram(binwidth = .02) +
-  ggtitle("Histogram of Defensive 3rd Down Conversion %") 
-ggplot(data, aes(x = X4thDownConvPct, fill = Conference)) + geom_histogram(binwidth = .05) +
-  ggtitle("Histogram of 4th Down Conversion %") 
-ggplot(data, aes(x = X4thDownConvPct, fill = Conference)) + geom_histogram(binwidth = .05) +
-  ggtitle("Histogram of Defensive 4th Down Conversion %") 
-ggplot(data, aes(x = CompPct, fill = Conference)) + geom_histogram(binwidth = .02) +
-  ggtitle("Histogram of Completion %") 
-ggplot(data, aes(x = PenPerGame, fill = Conference)) + geom_histogram(binwidth = 1) +
-  ggtitle("Histogram of Penalties per Game") 
-ggplot(data, aes(x = PenYardsPerGame, fill = Conference)) + geom_histogram(binwidth = 5) +
-  ggtitle("Histogram of Penalty Yards per Game") 
-ggplot(data, aes(x = OppFDPerGame, fill = Conference)) + geom_histogram(binwidth = 1) +
-  ggtitle("Histogram of Opponents First Downs per Game") 
-ggplot(data, aes(x = FDPerGame, fill = Conference)) + geom_histogram(binwidth = 1) +
-  ggtitle("Histogram of First Downs per Game") 
-ggplot(data, aes(x = NetYdsPunt, fill = Conference)) + geom_histogram(binwidth = 1) +
-  ggtitle("Histogram of Net Yards per Punt") 
-ggplot(data, aes(x = PassYdsPerAtt, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Pass Yards per Attempt") 
-ggplot(data, aes(x = PassYdsPerComp, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Pass Yards per Completion") 
-ggplot(data, aes(x = PassYdsPerGame, fill = Conference)) + geom_histogram(binwidth = 30) +
-  ggtitle("Histogram of Pass Yards per Game") 
-ggplot(data, aes(x = PassYdsPerAtt, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Defensive Pass Yards Allowed per Attempt") 
-ggplot(data, aes(x = PassYdsPerCompDef, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Defensive Pass Yards Allowed per Completion") 
-ggplot(data, aes(x = PassYdsPerGameDef, fill = Conference)) + geom_histogram(binwidth = 25) +
-  ggtitle("Histogram of Defensive Pass Yards Allowed per Game") 
-ggplot(data, aes(x = AvgPuntReturnYds, fill = Conference)) + geom_histogram(binwidth = 2) +
-  ggtitle("Histogram of Average Punt Return Yards") 
-ggplot(data, aes(x = OppRZScorePct, fill = Conference)) + geom_histogram(binwidth = .02) +
-  ggtitle("Histogram of Oppenent Red Zone Score %")
-ggplot(data, aes(x = OppPointsPerRZAtt, fill = Conference)) + geom_histogram(binwidth = .2) +
-  ggtitle("Histogram of Oppenent Red Zone Points per Attempt")
-ggplot(data, aes(x = RZScorePct, fill = Conference)) + geom_histogram(binwidth = .02) +
-  ggtitle("Histogram of Red Zone Score %")
-ggplot(data, aes(x = PointsPerRZAtt, fill = Conference)) + geom_histogram(binwidth = .2) +
-  ggtitle("Histogram of Red Zone Points per Attempt")
-ggplot(data, aes(x = RushYdsPerRushDef, fill = Conference)) + geom_histogram(binwidth = .3) +
-  ggtitle("Histogram of Defensive Rushing Yards per Rush")
-ggplot(data, aes(x = RushYdsPerRush, fill = Conference)) + geom_histogram(binwidth = .3) +
-  ggtitle("Histogram of Rushing Yards per Rush")
-ggplot(data, aes(x = RushYdsPerGame, fill = Conference)) + geom_histogram(binwidth = 20) +
-  ggtitle("Histogram of Defensive Rushing Yards per Game")
-ggplot(data, aes(x = SacksAllowed, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Sacks Allowed per Game")
-ggplot(data, aes(x = PPGDef, fill = Conference)) + geom_histogram(binwidth = 2.5) +
-  ggtitle("Histogram of Defensive Points Allowed per Game")
-ggplot(data, aes(x = PPG, fill = Conference)) + geom_histogram(binwidth = 2.5) +
-  ggtitle("Histogram of Points per Game")
-ggplot(data, aes(x = TFLAllowed, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Tackles for Loss Allowed per Game")
-ggplot(data, aes(x = Sacks, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Sacks per Game")
-ggplot(data, aes(x = TFLPG, fill = Conference)) + geom_histogram(binwidth = .5) +
-  ggtitle("Histogram of Tackles for Loss per Game")
-ggplot(data, aes(x = AvgTOP, fill = Conference)) + geom_histogram(binwidth = 1) +
-  ggtitle("Histogram of Average Time of Possesion")
-ggplot(data, aes(x = YdsPerPlayDef, fill = Conference)) + geom_histogram(binwidth = .25) +
-  ggtitle("Histogram of Average Defensive Yards Allowed per Play")
-ggplot(data, aes(x = YdsPerGameDef, fill = Conference)) + geom_histogram(binwidth = 20) +
-  ggtitle("Histogram of Average Defensive Yards Allowed per Game")
-ggplot(data, aes(x = YdsPerPlay, fill = Conference)) + geom_histogram(binwidth = .25) +
-  ggtitle("Histogram of Average Yards per Play")
-ggplot(data, aes(x = YdsPerGame, fill = Conference)) + geom_histogram(binwidth = 20) +
-  ggtitle("Histogram of Average Yards per Game")
-ggplot(data, aes(x = AvgTurnoverMargin, fill = Conference)) + geom_histogram(binwidth = .2) +
-  ggtitle("Histogram of Average Turnover Margin")
-ggplot(data, aes(x = WinPct, fill = Conference)) + geom_histogram(binwidth = .1) +
-  ggtitle("Histogram of Win %")
-ggplot(data, aes(x = Win...vs.Rank, fill = Conference)) + geom_histogram(binwidth = .1) +
-  ggtitle("Histogram of Win % vs. Ranked Opponents")
-ggplot(data, aes(x = Avg.Point.Differential, fill = Conference)) + geom_histogram(binwidth = 5) +
-  ggtitle("Histogram of Average Point Differential")
-
-data = data[data$Year != 2020,]
-
-############ Correlation plots #############
-
-# fbs independent
-corrplot(cor(data[data$Conference=="FBS Independent",! names(data) %in% c("Team","Conference","Year")],
-             method="spearman"), type='lower', tl.cex=.5, tl.srt=45, tl.col="black")
-title(main="Correlation for Notre Dame", adj=1)
-
-# all but fbs independent
-corrplot(cor(transform(data,Conference = as.numeric(Conference))[data$Conference!="FBS Independent",! names(data) %in% c("Team","Year")],
-             method="spearman"), type='lower', tl.cex=.5, tl.srt=45, tl.col="black")
-title(main="Correlation without Notre Dame", adj=1)
-
-# include fbs independent
-corrplot(cor(transform(data,Conference = as.numeric(Conference))[,! names(data) %in% c("Team","Year")],
-             method="spearman"), type='lower', tl.cex=.5, tl.srt=45, tl.col="black")
-title(main="Overall Correlation", adj=1)
-
-################## removing multicollinearity ##################
-
-red.data = data[, ! names(data) %in% c("Team","Games","Year","Avg.Point.Differential")]
-red.data$Conf.Champ = abs(red.data$Conf.Champ)
-
-model = glm(Selected ~., data=red.data, family="binomial")
-ld.vars = attributes(alias(model)$Complete)$dimnames[[1]]
-vifs = vif(model)
-
-test = vifs[,3]
-sort(test, decreasing=TRUE)[1]
-
-curr_vif = as.numeric(sort(test, decreasing=TRUE)[1])
-curr_name = names(sort(test, decreasing=TRUE)[1])
-while (curr_vif > 5) {
-  red.data = red.data[, ! names(red.data) %in% curr_name]
-  newmodel = glm(Selected ~., data=red.data, family="binomial")
-  newvif = vif(newmodel)
-  curr_vif = as.numeric(sort(newvif[,3], decreasing=TRUE)[1])
-  curr_name = names(sort(newvif[,3], decreasing=TRUE)[1])
-}
-
-red.model = glm(Selected ~., data=red.data, family="binomial")
-summary(red.model)
-
-########## with bayes glm ############
-
-model2 = bayesglm(Selected ~., data=red.data, family="binomial")
-summary(red.model)
-
-red.model2 = stepAIC(model2, trace=FALSE, direction="backward")
-summary(red.model2)
-
-red.data2 = red.data[,names(red.data) %in% names(red.model2$coefficients)[-1]]
->>>>>>> d3115a2761dba4762ef958e54798a7a85d45c7ad
 
 #converting conference to numbers for xgboost
 newdata$Conference <- as.numeric(factor(newdata$Conference)) 
@@ -331,7 +185,7 @@ for (i in 1:100) {
   xgb.pred <- predict(xgb.fit, test.data, reshape = T) #predicting on set aside train data
   xgb.pred <- as.data.frame(xgb.pred)
   pred_real <- cbind(xgb.pred, test.label)
-
+  
   pred_real$xgb.pred = as.numeric(pred_real$xgb.pred > .5)
   confusion <- table(pred_real)
   error = confusion[4]/(confusion[3] + confusion[4])
