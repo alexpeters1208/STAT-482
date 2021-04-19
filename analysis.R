204db03280dff3394ff2d5cad93882c8cd0d3fcf
CTaggie18 <ctaggie18@tamu.edu>
2021-04-18 19:38:26 -0500

    Random Forest done

--- analysis.R
+++ analysis.R
@@ -8,6 +8,7 @@ data = read.csv("football.csv")
 data$Conference = as.factor(data$Conference)
 data$Year = as.factor(data$Year)
 data$Selected = as.factor(data$Selected)
+##data$Yrs.Since.Last.Selected = as.factor(data$Yrs.Since.Last.Selected
 
 # Explorartory Histograms
 ggplot(data, aes(x = Games)) + geom_histogram(binwidth = 1, fill = 4) + 
@@ -45,27 +46,65 @@ removed = c("Team", "Year", "X3rdDownConvPct", "X3rdDownConvPctDef", "OppFDPerGa
             "YdsPerGameDef", "PassYdsPerAtt", "PassYdsPerAttDef", "RushYdsPerRush",
             "Avg.Point.Differential", "PenPerGame", "OppRZScorePct", "RZScorePct",
             "SacksAllowed", "Sacks")
-my_removed = c("Team","Year","Games")
+cameron_removed = c("Team","Year","Games")
 
-newdata = data[,! names(data) %in% my_removed]
+newdata = data[,! names(data) %in% cameron_removed]
 
 corrplot(cor(transform(newdata,Conference = as.numeric(Conference)),
              method="spearman"), type='lower', tl.cex=.5, tl.srt=45, tl.col="black")
 title(main="Overall Correlation", adj=1)
 
-set.seed(2021)
-trainIndex <- createDataPartition(newdata$Selected, p = 5/6,
-                                  list = FALSE,
-                                  times = 1)
-train_data <- newdata[ trainIndex,]
-test_data <- newdata[-trainIndex,]
-
-control <- trainControl(method="repeatedcv", number=10, repeats=3, search="random")
-set.seed(2022)
-mtry <- sqrt(ncol(train_data))
-rf_random <- train(Selected~., data=train_data, method="rf", metric="Accuracy", tuneLength=15, trControl=control)
-print(rf_random)
-plot(rf_random)
-
-rf_probs<-predict(rf_random, newdata = test_data, type = "prob")
-##Alabama 2014 (accurate), Clemson 2019 (Accurate), Iowa 2015 (Not accurate) should be 2017 Bama, Notre Dame 2018 (Accurate)
+successful_years<-0
+all_but_four<-0
+r<-100
+for (k in 1:r) {
+  new_vec<-matrix(ncol=3)
+  #set.seed(k^3)
+  trainIndex <- createDataPartition(newdata$Selected, p = 5/6,
+                                    list = FALSE,
+                                    times = 1)
+  train_data <- newdata[ trainIndex,]
+  test_data <- newdata[-trainIndex,]
+  #train_data<-newdata[c(1:260,326:390),]
+  #test_data<-newdata[261:325,]
+  
+  control <- trainControl(method="repeatedcv", number=10, repeats=3, search="random")
+  mtry <- sqrt(ncol(train_data))
+  rf_random <- train(Selected~., data=train_data, method="rf", metric="Accuracy", tuneLength=15, trControl=control)
+  #print(rf_random)
+  #plot(rf_random)
+  
+  rf_probs<-predict(rf_random, newdata = test_data, type = "prob")
+  for (i in 1:65) {
+    if (rf_probs[i,2] >= 0.1) {
+      new_vec <- rbind(new_vec,c(as.numeric(row.names(rf_probs[i,])),rf_probs[i,]))
+    }
+  }
+  new_vec<-new_vec[-1,]
+  new_vec1<-unlist(new_vec[,1])
+  new_vec2<-unlist(new_vec[,2])
+  new_vec3<-unlist(new_vec[,3])
+  new_vec <- matrix(c(new_vec1,new_vec2,new_vec3),ncol=3,byrow=FALSE)
+  new_vec<-new_vec[order(new_vec[,3],decreasing=TRUE),]
+  print(new_vec)
+  for (j in 1:nrow(new_vec)) {
+    row_num<-as.numeric(unlist(new_vec[j,1])) - 65
+    if (data[row_num,2] == 1) {
+      cat(as.character(data[row_num,1]),"was selected in",as.character(data[row_num,"Year"]),"\n")
+    } else {
+      cat(as.character(data[row_num,1]),"was not selected in",as.character(data[row_num,"Year"]),"\n")
+    }
+  }
+  if (data[new_vec[1,1]-65,2] == 1 && data[new_vec[2,1]-65,2] == 1 && data[new_vec[3,1]-65,2] == 1 && data[new_vec[4,1]-65,2] == 1) {
+    successful_years <- successful_years + 1
+  }
+  else {
+    if (data[new_vec[1,1]-65,2] == 1 && data[new_vec[2,1]-65,2] == 1 && data[new_vec[3,1]-65,2] == 1 && data[new_vec[4,1]-65,2] == 0) {
+      all_but_four<-all_but_four+1
+    }
+  }
+  paste("\n")
+  paste("\n")
+}
+correct_pct<-(successful_years+all_but_four)/r
+correct_pct
